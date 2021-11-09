b7eb2994f611e542d40e1dcb16c0c981da26b184
carol <carolina_hatsue@hotmail.com>
2021-11-08 22:50:11 -0300

    update paginas jsp

--- IFOOD/WebContent/restaurantes.jsp
+++ IFOOD/WebContent/restaurantes.jsp
@@ -1,25 +1,33 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Cadastro - JSPs</title>
-<%@ include file="header.jsp" %>
+<title>Restaurantes</title>
+<%@ include file="header.jsp"%>
 </head>
 <body>
-    <div class="site-wrapper">
-      <div class="site-wrapper-inner">
-        <div class="container">
-		  <%@ include file="menu.jsp" %>
+	<div class="site-wrapper">
+		<div class="site-wrapper-inner">
+			<div class="container">
+				<%@ include file="menu.jsp"%>
 
-          <div class="inner-cover">
-            <h1 class="cover-heading">Restaurantes</h1>
-          </div>
-        </div>
-      </div>
+				<div class="inner-cover">
+					<h1 class="cover-heading">Restaurantes</h1>
+					<c:if test="${not empty msg }">
+						<div class="alert alert-success">${msg}</div>
+					</c:if>
+					<c:if test="${not empty erro }">
+						<div class="alert alert-danger">${erro}</div>
+					</c:if>
+				</div>
+			</div>
+		</div>
 
-    </div>
-<%@ include file="footer.jsp" %>
+	</div>
+	<%@ include file="footer.jsp"%>
 </body>
 </html>
--- IFOOD/src/br/com/fiap/controller/CadastrarRestauranteServlet.java
+++ IFOOD/src/br/com/fiap/controller/CadastrarRestauranteServlet.java
@@ -7,34 +7,57 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.fiap.shycode.bean.Restaurant;
+import br.com.fiap.shycode.dao.RestaurantDAO;
+import br.com.fiap.shycode.exception.DBException;
+import br.com.fiap.shycode.factory.DAOFactory;
+
 /**
  * Servlet implementation class CadastrarRestauranteServlet
  */
-@WebServlet("")
+@WebServlet("/cadastro")
 public class CadastrarRestauranteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public CadastrarRestauranteServlet() {
-        // TODO Auto-generated constructor stub
-    }
-
+	private RestaurantDAO dao;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		dao = DAOFactory.getRestaurantDAO();
+	}
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		String acao = request.getParameter("cadastro");
+//		
+//	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		try{
+			String nome = request.getParameter("nome");
+			float preco = Float.parseFloat(request.getParameter("valor"));
+			
+			Restaurant restaurant = new Restaurant(1, nome, preco); 
+			dao.insert(restaurant);
+			
+			request.setAttribute("msg", "Produto cadastrado!");
+		}catch(DBException db) {
+			db.printStackTrace();
+			request.setAttribute("erro", "Erro ao cadastrar");
+		}catch(Exception e){
+			e.printStackTrace();
+			request.setAttribute("erro","Por favor, valide os dados");
+		}
+		request.getRequestDispatcher("cadastro.jsp").forward(request, response);
 	}
+	
+	
 
 }
